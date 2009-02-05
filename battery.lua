0faf6a797d84007f42745f12e263176d6f13c8a6
David Soulayrol <david.soulayrol@gmail.com>
2009-02-05 23:22:59 +0100

    Simplified Battery module. Special markups and icons management should now be handled by events.

--- battery.lua
+++ battery.lua
@@ -68,7 +68,7 @@ STATUS_DISCHARGING = 'v'
 -- activity on the battery, <code>STATUS_CHARGING</code> if the
 -- battery is in charge, or <code>STATUS_DISCHARGING</code> if the
 -- battery is currently the only power supply.</li>
---  </ul>
+-- </ul>
 -- @class table
 -- @name BatteryProvider
 BatteryProvider = flaw.provider.Provider:new{ type = _NAME }
@@ -78,16 +78,17 @@ BatteryProvider.data.status = ''
 --- Callback for provider refresh.
 function BatteryProvider:do_refresh()
    local load
-   local raw_status
    local fcur = io.open("/sys/class/power_supply/" .. self.id .. "/charge_now")
    local fcap = io.open("/sys/class/power_supply/" .. self.id .. "/charge_full")
-   local f_status = io.open("/sys/class/power_supply/" .. self.id .. "/status")
    if fcur ~= nil and fcap ~= nil then
       load = math.floor(fcur:read() * 100 / fcap:read())
       if load > 100 then load = 100 end
       fcur:close()
       fcap:close()
    end
+
+   local f_status = io.open("/sys/class/power_supply/" .. self.id .. "/status")
+   local raw_status
    if f_status ~= nil then
       raw_status = f_status:read()
       f_status:close()
@@ -97,25 +98,10 @@ function BatteryProvider:do_refresh()
 
    if raw_status:match("Charging") then
       status = STATUS_CHARGING
-
    elseif raw_status:match("Discharging") then
       status = STATUS_DISCHARGING
-
-      if tonumber(load) > 25 and tonumber(load) < 75 then
-         load = flaw.helper.format.set_fg("#e6d51d", load)
-      elseif tonumber(load) < 25 then
-         if tonumber(load) < 10 then
-            naughty.notify{
-               title = "Battery Warning",
-               text = "Battery low! " .. load .. "% left!",
-               timeout = 5,
-               position = "top_right",
-               fg = beautiful.fg_focus,
-               bg = beautiful.bg_focus}
-         end
-         load = flaw.helper.format.set_fg("#ff6565", load)
-      end
    end
+
    self.data.load = load
    self.data.status = status
 end
@@ -148,20 +134,8 @@ flaw.gadget.register(
 )
 
 -- An icon gadget prototype for battery status display.
-BatteryIconGadget = flaw.gadget.IconGadget:new{ type = _NAME .. '.imagebox' }
-
-function BatteryIconGadget:update()
-   if self.provider ~= nil then
-      self.provider:refresh()
-      if self.provider.data.status ~= self.status then
-         self.status = self.provider.data.status
-         self.widget.image = self.images[self.status]
-      end
-   end
-end
-
 flaw.gadget.register(
-   BatteryIconGadget,
+   flaw.gadget.IconGadget:new{ type = _NAME .. '.imagebox' },
    BatteryProviderFactory,
    { status = STATUS_UNKNOWN }
 )
