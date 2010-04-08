b68155973b335972d7dfb3e2cdc413fe52b98eec
David Soulayrol <david.soulayrol@gmail.com>
2010-04-08 15:56:03 +0200

    Added immediate update of gadgets at startup.

--- gadget.lua
+++ gadget.lua
@@ -1,5 +1,5 @@
 -- flaw, a Lua OO management framework for Awesome WM widgets.
--- Copyright (C) 2009 David Soulayrol <david.soulayrol AT gmail DOT net>
+-- Copyright (C) 2009, 2010 David Soulayrol <david.soulayrol AT gmail DOT net>
 
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -259,6 +259,9 @@ function new(type, id, gopt, wopt)
 
    -- Start monitoring.
    g:register(gopt.delay)
+
+   -- Initial display.
+   g:update()
 
    return add(g)
 end
